1c569aa0821d47e2c07841f0d7c859c0a22ab1a3
arthurfulldev <arturo.fulldev@gmail.com>
2019-02-14 01:02:12 -0600

    modificaciones

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GetMore Maqueta
 Requerimiento de entrevista para GetMore
 
-##
+## Recursos
 * [Link Maqueta Original](http://alegpaez.com/servicios/)
 * [JsonMock](http://demo6292426.mockable.io/more-store)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 $(document).ready( function (){
+
+    $('.initNav').on('click', function(){
+        $('.customNav').toggle();
+    })
+
     $.ajax({
         url: "http://demo6292426.mockable.io/more-store",
         success: function(data){
             resultado = data.filter( res => res.isActive == true ).slice(0, 3)
-            console.log(resultado);
+            resultado.forEach( item => {
+                console.log( item.nameOfProduct)
+                console.log( item.price)
+                console.log('----');
+            });
         }
     })
 })
